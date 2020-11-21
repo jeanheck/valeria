f4057d4cfcf37d8188bead66d4d6bf11644aa895
Jean Carlos heck <jeancarlosheck@gmail.com>
2020-11-21 12:56:28 -0300

    create bankOption component

--- src/components/resources/Bank.vue
+++ src/components/resources/Bank.vue
@@ -1,30 +1,18 @@
 <template>
   <div>
     <h1>Bank</h1>
-    <button v-on:click="getOneGold()" :disabled="this.$store.state.game.phase != 'ACTION_PHASE'">Get 1 Gold</button>
-    <button v-on:click="getOneMagic()" :disabled="this.$store.state.game.phase != 'ACTION_PHASE'">Get 1 Magic</button>
-    <button v-on:click="getOneForce()" :disabled="this.$store.state.game.phase != 'ACTION_PHASE'">Get 1 Force</button>
+    
+    <BankOption v-for="resource in Object.keys(this.$store.state.player.resources).filter(resource => resource != 'victory')" :key="resource" :resource="resource" />
   </div>
 </template>
 
 <script>
-import { doingOneAction } from '../../assets/js/phasesController.js'
+import BankOption from './BankOption'
 
 export default {
   name: 'Bank',
-  methods: {
-    getOneGold() {
-      this.$store.commit('addResource', {type: 'gold', value: 1})
-      doingOneAction(this.$store);
-    },
-    getOneMagic() {
-      this.$store.commit('addResource', {type: 'magic', value: 1})
-      doingOneAction(this.$store);
-    },
-    getOneForce() {
-      this.$store.commit('addResource', {type: 'force', value: 1})
-      doingOneAction(this.$store);
-    }
+  components: {
+    BankOption
   }
 }
 </script>
---
+++ src/components/resources/BankOption.vue
@@ -0,0 +1,27 @@
+<template>
+  <button v-on:click="getResource()" :disabled="this.$store.state.game.phase != 'ACTION_PHASE'">Get 1 <span>{{resource}}</span></button>
+</template>
+
+<script>
+import { doingOneAction } from '../../assets/js/phasesController.js'
+
+export default {
+  name: 'BankOption',
+  props: {
+    resource: String
+  },
+  methods: {
+    getResource() {
+      this.$store.commit('addResource', {type: this.resource, value: 1})
+      doingOneAction(this.$store);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  span {
+    text-transform: capitalize;
+  }
+</style>
