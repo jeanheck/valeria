41b3d4c5b767aaf768f2200b8023bdae01b1c602
Jean Carlos Heck <jeancarlosheck@gmail.com>
2021-02-28 13:44:07 -0300

    prototype layout

---
+++ src/Test.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+
+    <div style="width: 1900px; height: 100px; border: 1px solid green;">
+      <div style="width: 1000px; height: 100px; border: 1px solid yellow; float: left;"><!--bank-->
+        <div style="width: 100px; height: 90px; border: 1px solid gray; float: left;">
+          dices
+        </div>
+        <div style="width: 100px; height: 90px; border: 1px solid gold; float: left;">
+          get 1 gold
+        </div>
+        <div style="width: 100px; height: 90px; border: 1px solid blue; float: left;">
+          get 1 magic
+        </div>
+        <div style="width: 100px; height: 90px; border: 1px solid red; float: left;">
+          get 1 force
+        </div>
+      </div>
+      <div style="width: 690px; height: 100px; border: 1px solid orange; float: left;"><!--player-->
+        <div style="width: 100px; height: 90px; border: 1px solid gold; float: left;">
+          user gold
+        </div>
+        <div style="width: 100px; height: 90px; border: 1px solid blue; float: left;">
+          user magic
+        </div>
+        <div style="width: 100px; height: 90px; border: 1px solid red; float: left;">
+          user force
+        </div>
+        <div style="width: 100px; height: 90px; border: 1px solid purple; float: left;">
+          victory points
+        </div>
+        <div style="width: 180px; height: 90px; border: 1px solid gray; float: left;">
+          Active effects
+        </div>
+      </div>
+    </div>
+
+    <div style="width: 1010px; height: 810px; border: 1px solid blue; float: left;">
+      <div style="width: 1000px; height: 200px; border: 1px solid brown;">
+        <div style="width: 175px; height: 175px; border: 1px solid brown; float: right;">monster</div>
+        <div style="width: 175px; height: 175px; border: 1px solid brown; float: right;">monster</div>
+        <div style="width: 175px; height: 175px; border: 1px solid brown; float: right;">monster</div>
+        <div style="width: 175px; height: 175px; border: 1px solid brown; float: right;">monster</div>
+        <div style="width: 175px; height: 175px; border: 1px solid brown; float: right;">monster</div>
+      </div>
+      <div style="width: 1000px; height: 200px; border: 1px solid blue;">
+        <div style="width: 175px; height: 175px; border: 1px solid blue; float: right;">citizen</div>
+        <div style="width: 175px; height: 175px; border: 1px solid blue; float: right;">citizen</div>
+        <div style="width: 175px; height: 175px; border: 1px solid blue; float: right;">citizen</div>
+        <div style="width: 175px; height: 175px; border: 1px solid blue; float: right;">citizen</div>
+        <div style="width: 175px; height: 175px; border: 1px solid blue; float: right;">citizen</div>
+      </div>
+      <div style="width: 1000px; height: 200px; border: 1px solid blue;">
+        <div style="width: 175px; height: 175px; border: 1px solid blue; float: right;">citizen</div>
+        <div style="width: 175px; height: 175px; border: 1px solid blue; float: right;">citizen</div>
+        <div style="width: 175px; height: 175px; border: 1px solid blue; float: right;">citizen</div>
+        <div style="width: 175px; height: 175px; border: 1px solid blue; float: right;">citizen</div>
+        <div style="width: 175px; height: 175px; border: 1px solid blue; float: right;">citizen</div>
+      </div>
+      <div style="width: 1000px; height: 200px; border: 1px solid green;">
+        <div style="width: 175px; height: 175px; border: 1px solid green; float: right;">domain</div>
+        <div style="width: 175px; height: 175px; border: 1px solid green; float: right;">domain</div>
+        <div style="width: 175px; height: 175px; border: 1px solid green; float: right;">domain</div>
+        <div style="width: 175px; height: 175px; border: 1px solid green; float: right;">domain</div>
+        <div style="width: 175px; height: 175px; border: 1px solid green; float: right;">domain</div>
+      </div>
+    </div>
+
+    <div style="width: 200px; height: 810px; border: 1px solid purple; float: left;">
+      <div style="width: 190px; height: 200px; border: 1px solid purple;">
+        duke
+      </div>
+      <div style="width: 190px; height: 200px; border: 1px solid purple;">
+        duke info
+      </div>
+    </div>
+
+    <div style="width: 220px; height: 810px; border: 1px solid green; float: left;">
+      builded domains
+    </div>
+    <div style="width: 220px; height: 810px; border: 1px solid blue; float: left;">
+      buyed citizens
+    </div>
+    <div style="width: 220px; height: 810px; border: 1px solid brown; float: left;">
+      kiled monsters
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+</style>
