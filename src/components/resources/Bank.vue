40f9293833306a87108b0b19ac5f59d3d89494e0
Jean Carlos Heck <jeancarlosheck@gmail.com>
2021-02-28 20:47:34 -0300

    add bootstrap-vue

--- src/components/resources/Bank.vue
+++ src/components/resources/Bank.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
     <h1>Bank</h1>
+
+    <div>
+      <b-button size="sm" class="mb-2">
+        <b-icon icon="gear-fill" aria-hidden="true"></b-icon> Settings
+      </b-button>
+      <br>
+      <b-button variant="primary" class="mb-2">
+        Pay now <b-icon icon="credit-card" aria-hidden="true"></b-icon>
+      </b-button>
+      <br>
+      <b-button variant="outline-info" class="mb-2">
+        <b-icon icon="power" aria-hidden="true"></b-icon> Logout
+      </b-button>
+      <br>
+      <b-button size="lg" variant="primary" class="mb-2">
+        <b-icon icon="question-circle-fill" aria-label="Help"></b-icon>
+      </b-button>
+    </div>
     
     <BankOption v-for="resource in this.resources" :key="resource" :resource="resource" :ref="resource" />
   </div>
